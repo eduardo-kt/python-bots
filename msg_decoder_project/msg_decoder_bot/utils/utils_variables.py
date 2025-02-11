77e97dc82a2adf94e1eaf739680e492bc5e97e9f
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-02-11 17:12:17 -0300

    Adicionei variáveis URL_SOURCE e URL_TRADUTOR no arquivo utils_variables.py

---
+++ msg_decoder_project/msg_decoder_bot/utils/utils_variables.py
@@ -0,0 +1,14 @@
+# URLs
+URL_SOURCE = (
+    "https://pathfinder.automationanywhere.com/challenges/"
+    "AutomationAnywhereLabs-Translate.html?_gl=1*uoc654*_g"
+    "cl_au*Mjg2MDA3MjczLjE3Mzg5Mjc2MjA.*_ga*MTE4MDQzMTk1NC"
+    "4xNzM4OTI3NjIw*_ga_DG1BTLENXK*MTczOTI3MjQ4My4zLjEuMTc"
+    "zOTMwNDU0Ni41OC4wLjE4NTcwMDM5MzI."
+)
+
+URL_TRADUTOR = "https://translate.glosbe.com/bg-en"
+
+# SELECTORS
+
+
