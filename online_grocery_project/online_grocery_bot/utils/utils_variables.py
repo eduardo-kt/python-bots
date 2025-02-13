7657380196d2e85e563effba975255518da60466
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-02-13 07:24:04 -0300

    Adicionei variável 'URL_SOURCE' em 'utils_variables.py'

--- online_grocery_project/online_grocery_bot/utils/utils_variables.py
+++ online_grocery_project/online_grocery_bot/utils/utils_variables.py
@@ -2,3 +2,10 @@ from datetime import datetime
 
 # variáveis
 LOGFILE_KEY_NAME = datetime.now().strftime("%Y%m%d%H%M")
+
+URL_SOURCE = (
+    "https://pathfinder.automationanywhere.com/challenges/Automatio"
+    "nAnywhereLabs-ShoppingList.html?_gl=1*zvbndc*_gcl_au*Mjg2MDA3M"
+    "jczLjE3Mzg5Mjc2MjA.*_ga*MTE4MDQzMTk1NC4xNzM4OTI3NjIw*_ga_DG1BT"
+    "LENXK*MTczOTQzODQ0My41LjAuMTczOTQzODQ0Ni41Ny4wLjc5NDIxNTU2MQ.."
+)
