f40ff860666af47667d8bc396523b535416bd078
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-02-14 14:52:23 -0300

    Modifiquei arquivos de exclusão em build.ps1. Adicionei .venv e .env.

--- msg_decoder_project/msg_decoder_bot/build.ps1
+++ msg_decoder_project/msg_decoder_bot/build.ps1
@@ -1,3 +1,3 @@
-$exclude = @("venv", "msg_decoder_bot.zip")
+$exclude = @(".venv", ".env", "msg_decoder_bot.zip")
 $files = Get-ChildItem -Path . -Exclude $exclude
 Compress-Archive -Path $files -DestinationPath "msg_decoder_bot.zip" -Force
