a7394640dccd4ea63ff67d27125ff3da5df7b705
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-02-14 14:26:56 -0300

    Alterei arquivos excluídos pelo build.ps1.
    Incluí .venv e .env.

--- online_grocery_project/online_grocery_bot/build.ps1
+++ online_grocery_project/online_grocery_bot/build.ps1
@@ -1,3 +1,3 @@
-$exclude = @("venv", "online_grocery_bot.zip")
+$exclude = @(".venv", ".\.env", "online_grocery_bot.zip")
 $files = Get-ChildItem -Path . -Exclude $exclude
 Compress-Archive -Path $files -DestinationPath "online_grocery_bot.zip" -Force
